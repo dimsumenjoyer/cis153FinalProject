44f512d03c07707c05104ea6396b528f9a529a2b
文及信 Văn Cấp Tín (Victor) <62981175+dimsumenjoyer@users.noreply.github.com>
2023-12-05 12:25:23 -0500

    Add files via upload

--- cis153finalproject.py
+++ cis153finalproject.py
@@ -9,59 +9,42 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def testing():
-    #covid_data_file = pd.read_csv("usa_county_wise.csv")
-    #print(covid_data_file.head(2500))
-    #print(covid_data_file)
-    #print(covid_data_file.to_string()) # IO takes about 5-10 minutes. Don't print this unless you absolutely needed.
-    #print(covid_data_file.columns)
-    #columns_needed = ["Province_State", "Country_Region", "Date", "Confirmed", "Deaths"]
-    return
-
-def covid_data_dates():
-    columns_needed = ["Date"]
+def covid_dataframe():
+    columns_needed = ["Province_State", "Country_Region", "Date", "Confirmed", "Deaths"]
     covid_data_file = pd.read_csv("usa_county_wise.csv", usecols = columns_needed)
+    return covid_data_file
+
+def covid_data_dates(covid_data_file):
     unordered_dates = set(covid_data_file["Date"].unique()) # eliminating duplicate dates
     dates = sorted([pd.to_datetime(date) for date in unordered_dates])
-    #print(dates)
     return dates
 
-def covid_data_infected():
-    columns_needed = ["Date", "Confirmed"]
-    covid_data_file = pd.read_csv("usa_county_wise.csv", usecols = columns_needed)
+def covid_data_infected(covid_data_file):
     covid_data_file["Date"] = pd.to_datetime(covid_data_file["Date"])
     infected_cases = covid_data_file.groupby("Date")["Confirmed"].sum()
-    #print(infected_cases)
     return infected_cases
 
-def covid_data_deaths():
-    columns_needed = ["Date", "Deaths"]
-    covid_data_file = pd.read_csv("usa_county_wise.csv", usecols = columns_needed)
+def covid_data_deaths(covid_data_file):
     death_count = covid_data_file.groupby("Date")["Deaths"].sum()
-    #print(death_count)
     return death_count
 
-
-def covid_data_recovered():
-    columns_needed_names = ["Date", "Confirmed", "Deaths"]
-    covid_data_file = pd.read_csv("usa_county_wise.csv", usecols = columns_needed_names)
+def covid_data_recovered(covid_data_file):
     covid_data_file["Recoveries"] = covid_data_file["Confirmed"] - covid_data_file["Deaths"]
     recoveries = covid_data_file.groupby("Date")["Recoveries"].sum()
-    #print(recoveries)
     return recoveries
 
 def plot_covid_data(dates, infected_cases, death_count, recoveries):
-    plt.title("COVID-19: Infected Cases, Death Counts, and Recoveries Over Time")
+    plt.title("COVID-19: Infected Cases, Death Counts, and Recoveries Over Time in USA")
     plt.xlabel("Time (Y-M)")
-    plt.ylabel("Number of Cases")
+    plt.ylabel("Number of Cases (in Thousands)")
     plt.grid()
     plt.plot(dates, infected_cases / 1000, color = "orange", label = "Infected Cases")
     plt.plot(dates, death_count / 1000, color = "red", label = "Deaths")
     plt.plot(dates, recoveries / 1000, color = "green", label = "Recoveries")
-    #plt.autofmt_xdate()
     plt.legend()
     plt.show()
-    #print(dates, infected_cases, death_count, recoveries)
     return
 
-plot_covid_data(covid_data_dates(), covid_data_infected(), covid_data_deaths(), covid_data_recovered())
+x = covid_dataframe()
+
+plot_covid_data(covid_data_dates(x), covid_data_infected(x), covid_data_deaths(x), covid_data_recovered(x))
